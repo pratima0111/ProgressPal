46f5865eee3bf6c970a917dcf45a37610d66b8c0
Pratima Singh <gk1137@myamu.ac.in>
2023-04-29 01:32:56 +0530

    initial rest api

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 //entry point to our server
 
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+connectDB()
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+
 app.use('/api/goals', require('./routes/goalRoutes')) 
 
 app.use(errorHandler)
