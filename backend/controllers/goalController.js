88188b1eb1e900b208658584dba0e4de283d4872
Pratima Singh <gk1137@myamu.ac.in>
2023-05-09 16:32:10 +0530

    init project

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -36,15 +36,14 @@ const updateGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal not found')
     }
 
-    const user = await User.findById(req.user.id)
     // Check for user
-    if(!user){
+    if(!req.user){
         res.status(401)
         throw new Error('User not found')
     }
 
     //Make sure the logged in user matches the goal's user
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401)
         throw new Error('User not authorized!')
     }
@@ -61,15 +60,14 @@ const deleteGoal = asyncHandler(async (req, res)=> {
         res.status(400)
         throw new Error('Goal not found')
     }
-    const user = await User.findById(req.user.id)
     // Check for user
-    if(!user){
+    if(!req.user){
         res.status(401)
         throw new Error('User not found')
     }
 
     //Make sure the logged in user matches the goal's user
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401)
         throw new Error('User not authorized!')
     }
